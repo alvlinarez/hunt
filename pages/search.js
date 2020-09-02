34dc0284a8b3ff9fd785c540c5fe6d3b1964f838
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-09-01 21:07:27 -0500

    Search page added

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { ProductContext } from '../context/product/ProductContext';
+import Layout from '../components/layout/Layout';
+import {
+  ProductList,
+  ProductListContainer,
+  ProductListUl
+} from '../styles/pages/indexStyles';
+import ProductDetails from '../components/layout/ProductDetails';
 
 const Search = () => {
-  return <div></div>;
+  const router = useRouter();
+  const {
+    query: { q }
+  } = router;
+
+  const productContext = useContext(ProductContext);
+  const { products, getProducts } = productContext;
+
+  // Array of searched products
+  const [res, setRes] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+    if (products) {
+      const search = q.toLowerCase();
+      const filter = products.filter((product) => {
+        return (
+          product.name.toLowerCase().includes(search) ||
+          product.description.toLowerCase().includes(search) ||
+          product.creator.name.toLowerCase().includes(search)
+        );
+      });
+      setRes(filter);
+    }
+  }, [q]);
+
+  return (
+    <Layout>
+      <ProductList>
+        <ProductListContainer>
+          <ProductListUl>
+            {res && Object.keys(res).length <= 0 ? (
+              <h2>Sorry, no results :(</h2>
+            ) : (
+              res.map((product) => (
+                <ProductDetails key={product.id} product={product} />
+              ))
+            )}
+          </ProductListUl>
+        </ProductListContainer>
+      </ProductList>
+    </Layout>
+  );
 };
 
 export default Search;
